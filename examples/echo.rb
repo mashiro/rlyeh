6d10799e0924eaa7ea8f2c0a777e53e20c2ac786
mashiro <mail@mashiro.org>
2012-02-24 02:46:59 +0900

    エコーサーバ追加

---
+++ examples/echo.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+$LOAD_PATH.unshift 'lib', '../lib'
+require 'rlyeh'
+
+class Echo < Rlyeh::Base
+  def call(env)
+    conn = env.connection
+    conn.send_data env.message
+  end
+end
+
+Rlyeh.run Echo
