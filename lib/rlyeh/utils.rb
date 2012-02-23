1c6b8bf59f28c45d9f48b1e7d7d2bb85121ad5c3
mashiro <mail@mashiro.org>
2012-02-24 02:01:11 +0900

    メソッド生成ヘルパ追加

--- lib/rlyeh/utils.rb
+++ lib/rlyeh/utils.rb
@@ -8,6 +8,17 @@ module Rlyeh
       def extract_options!(args)
         args.last.is_a?(::Hash) ? args.pop : {}
       end
+
+      def generate_method(obj, name, method = nil, &block)
+        name = name.to_s
+        method ||= block
+        obj.instance_eval do
+          define_method name, &method
+          method = instance_method name
+          remove_method name
+          method
+        end
+      end
     end
   end
 end
