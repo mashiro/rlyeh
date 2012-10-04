6f0c78ea1c853fe953f14ea076eda86384bc3639
mashiro <mail@mashiro.org>
2012-10-04 19:37:30 +0900

    処理をちょっとまとめた

--- lib/rlyeh/target.rb
+++ lib/rlyeh/target.rb
@@ -7,21 +7,30 @@ module Rlyeh
       @target = target
     end
 
-    def send_message(command, message, prefix = nil)
-      prefix = {:nick => prefix} unless prefix.is_a?(Hash)
+    def send_message(command, message, options = {})
+      prefix = options[:prefix]
+      prefix = {:nick => prefix} if prefix && !prefix.is_a?(::Hash)
       @env.connection.send_message command, @target, ":#{message}", :prefix => prefix
-    end
-
-    def privmsg(message, prefix = nil)
-      send_message 'PRIVMSG', message, prefix
-    end
-
-    def notice(message, prefix = nil)
-      send_message 'NOTICE', message, prefix
     end
 
     def numeric_reply(type, *args)
       @env.connection.send_numeric_reply type, @target, *args, :prefix => {:servername => server_name}
+    end
+
+    def msg(message, options = {})
+      notice = options.delete(:notice) { false }
+      command = notice ? 'NOTICE' : 'PRIVMSG'
+      send_message command, message, options
+    end
+
+    def privmsg(message, options = {})
+      options[:notice] = false
+      msg message, options
+    end
+
+    def notice(message, options = {})
+      options[:notice] = true
+      msg message, options
     end
 
     private
